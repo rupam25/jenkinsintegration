3b35a752453cf79edc14cfb38dce1ff6d072d507
user <user@192.168.0.131>
2018-01-11 17:09:51 +0530

    first commit

---
+++ AProjectforIntegration/src/TestCases/TC_02.java
@@ -0,0 +1,29 @@
+
+package TestCases;
+
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class TC_02 {
+
+@Test
+public void testcase1()throws InterruptedException  {
+	System.setProperty("webdriver.chrome.driver", "E:\\PracticeWorkspace\\AProjectforIntegration\\Driver\\chromedriver.exe");
+
+	ChromeDriver driver= new ChromeDriver();
+	driver.get("http://www.faceook.com");
+	driver.manage().window().maximize();
+	Thread.sleep(5000);
+	driver.findElement(By.id("email")).sendKeys("rupam_life@yahoo.co.in");
+	Thread.sleep(2000);
+	driver.findElement(By.id("pass")).sendKeys("asl@1234");
+	driver.quit();
+	//Thread.sleep(5000);
+	//driver.findElement(By.xpath("//*[@type=\"submit\"]")).click();
+	//Thread.sleep(2000);
+}
+
+
+
+}
